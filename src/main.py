7aac8d6b5bb3acc65203f979d9140f3ebc8e2fe5
Dhwaj <dhwaj.kothari@yahoo.in>
2023-08-24 19:14:45 +0530

    resources added

--- src/main.py
+++ src/main.py
@@ -12,6 +12,13 @@ screen = pygame.display.set_mode((win_width, win_height),
 # set title
 pygame.display.set_caption('Irregular Trooper')
 
+# # Here we load the image we want to
+# # use
+# Icon = pygame.image.load('gfglogo.png')
+ 
+# # We use set_icon to set new icon
+# pygame.display.set_icon(Icon)
+
 # game loop
 running = True
 
@@ -19,6 +26,18 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+
+    color = (255,255,0)
+   
+    # Drawing Rectangle
+    pygame.draw.rect(screen, color,
+                    pygame.Rect(30, 30, 60, 60))
+    
+    # The pygame.display.flip() method is used
+    # to update content on the display screen
+    pygame.display.flip()
+
 
 # quit pygame after closing window
 pygame.quit()
